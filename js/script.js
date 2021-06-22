7f3e40961287d46edce46c4dbfd3670d3577a6ed
Arlesson Menezes <arlesson_10@hotmail.com>
2021-06-22 16:37:48 -0300

    Calculadora

---
+++ js/script.js
@@ -0,0 +1,71 @@
+function Calculadora(display, btnClear) {
+
+    this.display = display 
+    this.btnClear = btnClear
+
+    this.inicia = () => {
+        this.cliqueBotoes();
+        this.pressionaEnter();
+    }
+
+    this.realizarConta = () => {
+        let conta = this.display.value;
+
+        try {
+            conta = eval(conta);
+            if(!conta){
+                alert('Conta inválida');
+                return;
+            }
+            this.display.value = String(conta);
+        } catch(e) {
+            alert('Conta inválida');
+            return;
+        }
+    }
+
+    this.pressionaEnter = () => {
+        this.display.addEventListener('keyup', e => {
+            if(e.keyCode === 13) {
+                this.realizarConta();;
+            }
+        })
+    }
+
+    this.deleteOne = () => {
+        this.display.value = this.display.value.slice(0, -1);
+    }
+
+    this.clearDisplay = () =>{
+        this.display.value = '';
+    }
+
+    this.cliqueBotoes = () => {
+        document.addEventListener('click', (e) => {
+            const el = e.target;
+            if(el.classList.contains('btn-num')){
+                this.btnParaDisplay(el.innerText);
+            }
+            if(el.classList.contains('btn-clear')){
+                this.clearDisplay();
+            }
+            if(el.classList.contains('btn-del')){
+                this.deleteOne();
+            }
+            if(el.classList.contains('btn-eq')){
+                this.realizarConta();
+            }
+
+            this.display.focus();
+        });
+    };
+
+    this.btnParaDisplay = (valor) => {
+        this.display.value += valor; 
+    }
+
+}
+const calculadora = new Calculadora()
+calculadora.display = document.querySelector('.display')
+calculadora.btnClear = document.querySelector('.btn-clear');
+calculadora.inicia();
